a1ed2eca79d7ac74079b33927b5a8bbbce42b793
IdeaSeeker <stroganov.n.36@gmail.com>
2022-03-12 23:45:39 +0300

    add host and port to client cli

--- SocketServer/client.py
+++ SocketServer/client.py
@@ -3,11 +3,9 @@ import argparse
 
 from util import read_from_socket
 
-server_port = 12321
-
-def make_request(filename: str):
+def make_request(host, port, filename):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', server_port))
+    client_socket.connect((host, port))
     client_socket.sendall(bytes(f'GET /{filename} HTTP/1.1\r\n\r\n', 'utf-8'))
 
     print(read_from_socket(client_socket))
@@ -16,6 +14,10 @@ def make_request(filename: str):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('host', type=str)
+    parser.add_argument('port', type=int)
     parser.add_argument('filename', type=str)
+
     args = parser.parse_args()
-    make_request(args.filename)
+
+    make_request(args.host, args.port, args.filename)
