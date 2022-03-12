38b536d7fbcb217bf931281c97ed1c3212b076d8
IdeaSeeker <stroganov.n.36@gmail.com>
2022-03-12 23:31:44 +0300

    update client

--- SocketServer/client.py
+++ SocketServer/client.py
@@ -1,14 +1,21 @@
 import socket
+import argparse
 
 server_port = 12321
 
-client_socket = socket.socket()
-client_socket.connect(('localhost', server_port))
+def make_request(filename: str):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', server_port))
+    client_socket.sendall(bytes(f'GET /{filename} HTTP/1.1\r\n\r\n', 'utf-8'))
 
-filename = 'C:/Users/Xiaomi/Desktop/Projects/Python/gen.py'
-client_socket.sendall(bytes(f'GET /{filename} HTTP/1.1\r\n\r\n', 'utf-8'))
+    message = client_socket.recv(1024)
 
-data = client_socket.recv(1024).decode('utf-8')
-client_socket.close()
+    print(bytes(message).decode('utf-8'))
 
-print(data)
+    client_socket.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str)
+    args = parser.parse_args()
+    make_request(args.filename)
