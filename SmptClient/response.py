9ce4c6730335a19cdfde057c75e7121e7b629736
IdeaSeeker <stroganov.n.36@gmail.com>
2022-03-26 19:34:35 +0300

    remove unused code

--- SmptClient/response.py
+++
@@ -1,8 +0,0 @@
-def bad_request_response():
-    return b'HTTP/1.1 400 Bad Request\r\n\r\n'
-
-def not_found_response():
-    return b'HTTP/1.1 404 Not Found\r\n\r\n'
-
-def ok_response(content):
-    return bytes(f'HTTP/1.1 200 OK\r\nContent-Length: {len(content)}\r\n\r\n{content}', 'utf-8')
